6800c0598039199729f10ed5e2533b4012e0f8d7
Jack Pipitone <pipitone.jack@gmail.com>
2016-11-11 17:17:17 -0500

    Added ability to create new hobbies and add resources

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -6,7 +6,23 @@ class HobbiesController < ApplicationController
 
   def show
     @hobby = Hobby.find(params[:id])
+    @resource = Resource.new
     @message = Message.new
+  end
+
+  def new
+    @hobby = Hobby.new
+  end
+
+  def create
+    @hobby = Hobby.create(hobby_params)
+    redirect_to hobbies_path
+  end
+
+  private
+
+  def hobby_params
+    params.require(:hobby).permit(:name)
   end
 
 end
--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,2 +1,15 @@
 class ResourcesController < ApplicationController
+
+  def create
+    @hobby = Hobby.find(params[:hobby_id])
+    @hobby.resources.create(resource_params)
+    redirect_to hobby_path(@hobby)
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:link)
+  end
+
 end
